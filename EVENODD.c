050e984d84fd7a866b8ddb13e1455bd6b974344d
GOKILADHARANISHYAMALA <31985290+GOKILADHARANISHYAMALA@users.noreply.github.com>
2017-09-15 15:49:46 +0530

    Create EVENODD.c

---
+++ EVENODD.c
@@ -0,0 +1,18 @@
+# CODESIMPLE
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int num;
+ scanf("%d",&num);
+ if(num%2==0)
+ {
+  printf("\neven");
+ }
+ else
+ {
+  printf("\nodd");
+ }
+}
+  
+ 
