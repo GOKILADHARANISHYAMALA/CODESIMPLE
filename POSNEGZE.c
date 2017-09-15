b68c018635b61f161df4f71394d2826b9175513f
GOKILADHARANISHYAMALA <31985290+GOKILADHARANISHYAMALA@users.noreply.github.com>
2017-09-15 15:34:35 +0530

    Create POSNEGZE.c

---
+++ POSNEGZE.c
@@ -0,0 +1,19 @@
+# CODESIMPLE
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int num;
+ scanf("%d",&num);
+ if(num==0)
+ {
+ printf("\nzero");
+ }
+ else if(num>0)
+ {
+ printf("\npositive");
+ }
+ else
+ {
+ printf("\nnegative");
+ }
